54957b9e172069e2c0754cbb12c67c6f3c2c0418
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-06-20 17:11:25 -0300

    fix: add PendingShares

--- internal/market/entity/order.go
+++ internal/market/entity/order.go
@@ -14,13 +14,14 @@ type Order struct {
 
 func NewOrder(id string, investor *Investor, asset *Asset, shares int, price float64, orderType string) *Order {
 	return &Order{
-		ID:           id,
-		Investor:     investor,
-		Asset:        asset,
-		Shares:       shares,
-		Price:        price,
-		OrderType:    orderType,
-		Status:       "OPEN",
-		Transactions: []*Transaction{},
+		ID:            id,
+		Investor:      investor,
+		Asset:         asset,
+		Shares:        shares,
+		PendingShares: shares,
+		Price:         price,
+		OrderType:     orderType,
+		Status:        "OPEN",
+		Transactions:  []*Transaction{},
 	}
 }
