03c6407e45453154ae588e606d4f944ba40b4622
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-06-21 15:09:49 -0300

    feat: add orders resource

---
+++ broker-server/src/orders/orders.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { InitTransactionDto, InputExecuteTransactionDto } from './order.dto';
+
+@Controller('wallets/:wallet_id/orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  all(@Param('wallet_id') wallet_id: string) {
+    return this.ordersService.all({ wallet_id });
+  }
+
+  @Post()
+  initTransaction(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: Omit<InitTransactionDto, 'wallet_id'>,
+  ) {
+    return this.ordersService.initTransaction({
+      ...body,
+      wallet_id,
+    });
+  }
+
+  /*   @Post('execute')
+  executeTransaction(@Body() body: InputExecuteTransactionDto) {
+    this.ordersService.executeTransaction(body);
+  } */
+}
---
+++ broker-server/src/orders/orders.service.ts
@@ -0,0 +1,114 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { InitTransactionDto, InputExecuteTransactionDto } from './order.dto';
+import { OrderStatus, OrderType } from '@prisma/client';
+
+@Injectable()
+export class OrdersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  all(filter: { wallet_id: string }) {
+    return this.prismaService.order.findMany({
+      where: {
+        wallet_id: filter.wallet_id,
+      },
+      include: {
+        Transactions: true,
+        Asset: {
+          select: {
+            id: true,
+            symbol: true,
+          },
+        },
+      },
+      orderBy: {
+        updated_at: 'desc',
+      },
+    });
+  }
+
+  initTransaction(input: InitTransactionDto) {
+    return this.prismaService.order.create({
+      data: {
+        asset_id: input.asset_id,
+        wallet_id: input.wallet_id,
+        shares: input.shares,
+        partial: input.shares,
+        price: input.price,
+        type: input.type,
+        status: OrderStatus.PENDING,
+        version: 1,
+      },
+    });
+  }
+
+  async executeTransaction(input: InputExecuteTransactionDto) {
+    return this.prismaService.$transaction(async (prisma) => {
+      const order = await prisma.order.findFirstOrThrow({
+        where: { id: input.order_id },
+      });
+
+      await prisma.order.update({
+        where: { id: input.order_id, version: order.version },
+        data: {
+          partial: order.partial - input.negotiated_shares,
+          status: input.status,
+          Transactions: {
+            create: {
+              broker_transaction_id: input.broker_transaction_id,
+              related_investor_id: input.related_investor_id,
+              shares: input.negotiated_shares,
+              price: input.price,
+            },
+          },
+          version: { increment: 1 },
+        },
+      });
+
+      if (input.status === OrderStatus.CLOSED) {
+        await prisma.asset.update({
+          where: { id: order.asset_id },
+          data: {
+            price: input.price,
+          },
+        });
+
+        const walletAsset = await prisma.walletAsset.findUnique({
+          where: {
+            wallet_id_asset_id: {
+              asset_id: order.asset_id,
+              wallet_id: order.wallet_id,
+            },
+          },
+        });
+
+        if (walletAsset) {
+          await prisma.walletAsset.update({
+            where: {
+              wallet_id_asset_id: {
+                asset_id: order.asset_id,
+                wallet_id: order.wallet_id,
+              },
+            },
+            data: {
+              shares:
+                order.type === OrderType.BUY
+                  ? walletAsset.shares + order.shares
+                  : walletAsset.shares - order.shares,
+              version: { increment: 1 },
+            },
+          });
+        } else {
+          await prisma.walletAsset.create({
+            data: {
+              asset_id: order.asset_id,
+              wallet_id: order.wallet_id,
+              shares: input.negotiated_shares,
+              version: 1,
+            },
+          });
+        }
+      }
+    });
+  }
+}
